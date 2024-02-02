ae880bdcbb945c3fbd8727618789edab6d76c128
nowakpiotr <en.piotr@gmail.com>
2024-02-02 20:21:15 +0100

    base in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/app',
+  base: '/ghp-react-one',
   server: {
       https: true,
       port: 6363
